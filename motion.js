7597bc9980e4b320e767fec4781df93baf683951
ownyourzone <107634186+ownyourzone@users.noreply.github.com>
2026-01-25 00:56:26 +0200

    Add mouse and touchmove animations for logo

--- motion.js
+++ motion.js
@@ -1,1 +1,25 @@
 
+const logo = document.querySelector(".logo-wrapper");
+
+function animate(x, y) {
+  const rotateX = y * -12;
+  const rotateY = x * 12;
+
+  logo.style.transform = `
+    rotateX(${rotateX}deg)
+    rotateY(${rotateY}deg)
+  `;
+}
+
+window.addEventListener("mousemove", e => {
+  const x = (e.clientX / window.innerWidth) * 2 - 1;
+  const y = (e.clientY / window.innerHeight) * 2 - 1;
+  animate(x, y);
+});
+
+window.addEventListener("touchmove", e => {
+  const t = e.touches[0];
+  const x = (t.clientX / window.innerWidth) * 2 - 1;
+  const y = (t.clientY / window.innerHeight) * 2 - 1;
+  animate(x, y);
+});
